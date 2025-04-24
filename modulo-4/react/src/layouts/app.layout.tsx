ddbc7868c1d7b054db14c25ab39932c69be0b97d
rojasadrian012 <rojasadrian012@gmail.com>
2025-04-24 15:24:44 -0300

    create layout for navbar

--- modulo-4/react/src/layouts/app.layout.tsx
+++ modulo-4/react/src/layouts/app.layout.tsx
@@ -1,21 +1,30 @@
 import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
 import React from "react";
-
+import { routes } from "../routes";
+import { useNavigate } from "react-router-dom";
 
 export const AppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
     const [value, setValue] = React.useState(0);
+    const navigate = useNavigate();
+
+    const handleNavigationChange = (event: React.SyntheticEvent, newValue: number) => {
+        console.log(newValue);
+
+        setValue(newValue);
+        if (newValue === 0) {
+            navigate(`/${routes.gitHubMembersList}`);
+        } else if (newValue === 1) {
+            navigate(`/${routes.rickAndMortyCharacterList}`);
+        }
+    };
 
     return (
         <>
-            <Paper sx={{ position: 'fixed', top: 0, left: 0, right: 0 , height: '100dvh'}}>
-
+            <Paper sx={{ position: 'fixed', top: 0, left: 0, right: 0, height: '100dvh' }}>
                 <BottomNavigation
                     showLabels
                     value={value}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                        console.log(newValue);
-                    }}
+                    onChange={handleNavigationChange}
                 >
                     <BottomNavigationAction label="GitHub" />
                     <BottomNavigationAction label="Rick And Morty" />
@@ -23,7 +32,5 @@ export const AppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
                 {children}
             </Paper>
         </>
-
-    )
-
-}
+    );
+};
